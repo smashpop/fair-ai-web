63acc5e3924f72b64f6e3967d368d86ef2f07b50
limbyungki <lobster3@naver.com>
2024-01-27 14:57:38 +0900

    빌드

---
+++ dist/assets/event-IYF45O4Q.js
@@ -0,0 +1,1 @@
+import{s as t}from"./index-jcjGOSuj.js";function r(){return t({url:"/ai-event-list",method:"get"})}function i(e){return t({url:"/ai-event-last",method:"post",data:e})}export{r as a,i as f};
