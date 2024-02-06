1d8d84280bd2f194d18974982e49208d432ff18d
LIMBK <lobster3@naver.com>
2024-02-06 17:50:07 +0900

    svg 로고

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,7 +1,23 @@
-<script setup></script>
+<script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+function onClickImage() {
+  router.push('/home')
+}
+</script>
 
 <template>
-  <v-btn variant="text" href="/home">
-    <strong class="text-h4 font-weight-black">FAIR-AI</strong>
-  </v-btn>
+  <div class="pt-1" style="width: 260px; height: 48px">
+    <!-- 로고 -->
+    <v-img
+      src="@/assets/images/BI_new.svg"
+      class="cursor-pointer"
+      height="40"
+      alt="Logo"
+      @click="onClickImage"
+    />
+  </div>
 </template>
+
