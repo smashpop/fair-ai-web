67223dec568fe47721672291c04f26e34201b844
LIMBK <lobster3@naver.com>
2024-02-07 18:30:41 +0900

    빌드

---
+++ dist/assets/attachFile-Cpw5pdzo.js
@@ -0,0 +1,1 @@
+import{s as e}from"./index-u9-2_j-W.js";function a(t){return e({url:"/ai-attach-file",method:"post",data:t})}export{a as c};
--- dist/assets/attachFile-expzUfvW.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./index-1ienrsAh.js";function a(t){return e({url:"/ai-attach-file",method:"post",data:t})}export{a as c};
