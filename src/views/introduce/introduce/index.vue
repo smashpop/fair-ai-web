40137db4d62177c748de3d97f0fa9bc92e9154d6
Jang Seung-hun <jxxsxxh@naver.com>
2024-03-24 11:27:23 +0900

    ai 윤리

--- src/views/introduce/introduce/index.vue
+++ src/views/introduce/introduce/index.vue
@@ -1,11 +1,10 @@
 <script setup>
 import ContentV1 from '@/components/content/Content-v1'
-import ContentV2 from '@/components/content/Content-v2'
+//import ContentV2 from '@/components/content/Content-v2'
 </script>
 
 <template>
   <v-container fluid class="pa-0" style="min-width: 1360px;">
     <ContentV1 />
-    <ContentV2 />
   </v-container>
 </template>
